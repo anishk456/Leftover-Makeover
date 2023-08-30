5be5c1bd81ebb18b10ba5250b700cc0f540d19ec
anishk456 <143646873+anishk456@users.noreply.github.com>
2023-08-30 17:21:02 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+RECIPES_PATH = "./input/df_recipes.csv"
+PARSED_PATH = '/Users/anish/Documents/ML/honestcooking_all_recipes_parsed.csv'
+TFIDF_ENCODING_PATH = "./input/tfidf_encodings.pkl"
+TFIDF_MODEL_PATH = '/Users/anish/Documents/ML/tfidf_model.pkl'
